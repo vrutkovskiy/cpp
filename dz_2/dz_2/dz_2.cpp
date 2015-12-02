58328421f250f28871ec0819be93b90b40f7a2ec
vrutkovskiy <1153014@mail.ru>
2015-12-02 14:50:22 +0200

    dz_2 beta v0.1
    @maximla @sprightly1991 @AntonZaitsev @Swordix @igor-nplsk @marina-im
    check please

--- dz_2/dz_2/dz_2.cpp
+++ dz_2/dz_2/dz_2.cpp
@@ -1,91 +1,217 @@
-
 #include <iostream>				
 #include <cstring>
 using namespace std;
 
+#define BREAKER 1   //1 - program to work
+					//2 - to check write protection
+
 struct game_resource
 {
-	char *name = nullptr;
+	mutable char *name = nullptr;
 	mutable int life = 100;
-	unsigned *count = nullptr;
+	mutable unsigned *count = nullptr;
 };
 
 class Game1
 {
 	game_resource personage;
-	unsigned size = 0;
-	
+	mutable unsigned size = 0;
 public:	
-	//Game1() : personage{}, size(0) //default constructor	//does't working (don't understand)----???
-	//{}
+	Game1() : personage{}, size(0) //default constructor	
+	{}
 	void assign(const char *persname);
 	Game1(const char *persname)
 	{
 		assign(persname);
 	}
-#if menu1 == 1
-
+#if BREAKER == 1
+	Game1(const Game1 &newpers) :size(newpers.size)
+	{
+		personage.name = newpers.personage.name;
+		personage.life = newpers.personage.life;
+		newpers.personage.name = nullptr;
+		personage.count = newpers.personage.count;
+		++(*personage.count);			
+		newpers.size = 0;
+		newpers.personage.life = 0;
+	}
+	const Game1 &operator=(const Game1 &newpers)  
+	{
+		if (this == &newpers)
+			return *this;
+		else
+		{
+			size = newpers.size;
+			personage.name = newpers.personage.name;
+			personage.life = newpers.personage.life;
+			newpers.personage.name = nullptr;
+			personage.count = newpers.personage.count;
+			++(*personage.count);
+			newpers.size = 0;
+			newpers.personage.life = 0;
+			return *this;
+		}
+	}
 #endif
-#if menu1 == 2
 
-#endif
-#if menu1 == 3
+#if BREAKER == 2
 	Game1(const Game1 &newpers) = delete;
 	const Game1 &operator=(const Game1 &newpers) = delete;
 #endif
 	void get_game_res() const
 	{
-		cout <<"Name = "<< personage.name<< endl;
-		cout << "Life = " << personage.life << endl;
-		cout << "Count of personages = " << *personage.count << endl;
-		cout << "Length of personage name = " << size-1 << endl;
+		if (personage.name)
+			cout << "	Name = " << personage.name << "	Adress of pointer - " << &personage.name << endl;
+		else
+			cout << "	Name = ---"<< endl;
+		cout << "	Life = " << personage.life << endl;
+		if (personage.count)
+			cout << "	Count of personages = " << *personage.count << endl;
+		else
+			cout << "	Count of personages = 0" << endl;
+		if (size)
+			cout << "	Length of personage name = " << size - 1 << endl;
+		else
+			cout << "	Length of personage name = " << size << endl;
+		
 	}
 
 	~Game1()
 	{
 		--(*personage.count);
-		if (personage.name && *personage.count == 0)
+		if (personage.name && *personage.count == 1)
 		{
 			delete[] personage.name;
 		}
 		personage.name = nullptr;
 	}
 };
+
+class Game2
+{
+	game_resource personage;
+	mutable unsigned size = 0;
+public:
+	Game2() : personage{}, size(0) //default constructor	
+	{}
+	void assign(const char *persname);
+	Game2(const char *persname)
+	{
+		assign(persname);
+	}
+	Game2(const Game2 &newpers) :size(newpers.size)
+	{
+		personage.name = new char[size];
+		strcpy_s(personage.name, size, newpers.personage.name);
+		personage.life = newpers.personage.life;
+		
+	}
+	const Game2 &operator=(const Game2 &newpers)
+	{
+		if (this == &newpers)
+			return *this;
+		else
+		{
+			size = newpers.size;
+			personage.name = new char[size];
+			strcpy_s(personage.name, size, newpers.personage.name);
+			personage.life = newpers.personage.life;
+			return *this;
+		}
+	}
+
+	void get_game_res() const
+	{
+		if (personage.name)
+			cout << "	Name = " << personage.name << "	Adress of pointer - " << &personage.name << endl;
+		else
+			cout << "	Name = ---" << endl;
+		cout << "	Life = " << personage.life << endl;
+		if (size)
+			cout << "	Length of personage name = " << size - 1 << endl;
+		else
+			cout << "	Length of personage name = " << size << endl;
+
+	}
+
+	~Game2()
+	{
+		
+		if (personage.name)
+		{
+			delete[] personage.name;
+			personage.name = nullptr;
+		}
+	}
+};
+
 int main()
 {
-	cout << "MENU:" << endl;
-	cout << "1. Ordinary copying" << endl;
-	cout << "2. Remouving copying" << endl;
-	cout << "3. Prohibition of copying" << endl;
-	cout << "4. Exit" << endl;
-
 	int menu1;
-	fflush(stdin);
-	cin>>menu1;
 
-	if (menu1 > 0 && menu1 < 4)
-	{
-		Game1 pers1{ "Ivan" };
-		pers1.get_game_res();
-		Game1 pers2{"Fiodor"};
-		pers2.get_game_res();
-		
-		Game1 pers3 = pers2;
-		pers3.get_game_res();
-		pers2.get_game_res();
+	do{
+		cout << "MENU:" << endl;
+		cout << "1. Remouving copying (transfer of rights)" << endl;
+		cout << "2. Ordinary copying (cross ownership)" << endl;
+		cout << "3. Exit" << endl;
 
-		pers2 = pers1;
-		pers2.get_game_res();
-		pers1.get_game_res();
-	}
-	else 
-		if (menu1 == 4)
-			goto end;
+		fflush(stdin);
+		cin >> menu1;
+
+		if (menu1 == 1)
+		{
+			cout << "Creating personage 1:" << endl;
+			Game1 pers1{ "Ivan" };
+			pers1.get_game_res();
+			cout << "Creating personage 2:" << endl;
+			Game1 pers2{ "Fiodor" };
+			pers2.get_game_res();
+
+			cout << endl << "Copying personage 2 to 3 (CONSTRUCTOR pers3 = pers2):" << endl;
+			Game1 pers3 = pers2;
+			cout << "Personage 3:" << endl;
+			pers3.get_game_res();
+			cout << "Personage 2:" << endl;
+			pers2.get_game_res();
+
+			cout << endl << "Copying personage 1 to 2 (pers2 = pers1):" << endl;
+			pers2 = pers1;
+			cout << "Personage 2:" << endl;
+			pers2.get_game_res();
+			cout << "Personage 1:" << endl;
+			pers1.get_game_res();
+		}
 		else
-			cout << "Enter 1,2,3 or 4" << endl;
+			if (menu1 == 2)
+			{
+				cout << "Creating personage 1:" << endl;
+				Game2 pers1{ "Ivan" };
+				pers1.get_game_res();
+				cout << "Creating personage 2:" << endl;
+				Game2 pers2{ "Fiodor" };
+				pers2.get_game_res();
+
+				cout << endl << "Copying personage 2 to 3 (CONSTRUCTOR pers3 = pers2):" << endl;
+				Game2 pers3 = pers2;
+				cout << "Personage 3:" << endl;
+				pers3.get_game_res();
+				cout << "Personage 2:" << endl;
+				pers2.get_game_res();
+
+				cout << endl << "Copying personage 1 to 2 (pers2 = pers1):" << endl;
+				pers2 = pers1;
+				cout << "Personage 2:" << endl;
+				pers2.get_game_res();
+				cout << "Personage 1:" << endl;
+				pers1.get_game_res();
+			}
+			else
+				if (menu1 == 3)
+					break;
+				else
+					cout << "Enter 1,2 or 3" << endl;
+	} while (true);
 	
-	
-	end:
 	return 0;
 }
 void Game1::assign(const char *persname)
@@ -101,5 +227,19 @@ void Game1::assign(const char *persname)
 		personage.name = new char[size];
 		strcpy_s(personage.name, size, persname);
 		personage.count = new unsigned int(1);
+	}
+}
+void Game2::assign(const char *persname)
+{
+	if (personage.name)
+	{
+		delete[] personage.name;
+		personage.name = nullptr;
+	}
+	if (persname && *persname)
+	{
+		size = strlen(persname) + 1;
+		personage.name = new char[size];
+		strcpy_s(personage.name, size, persname);
 	}
 }
